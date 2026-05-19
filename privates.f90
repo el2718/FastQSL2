04d44c9e2f61551bf2e5165708ea38be76bde40e
el2718 <45861675+el2718@users.noreply.github.com>
2026-05-19 14:18:02 +0800

    Add files via upload

--- privates.f90
+++ privates.f90
@@ -5,15 +5,15 @@ real:: privates(0:9), Bp(0:2), CurlBp(0:2), Ap(0:2)
 ! for length
 privates(0)= 1.
 
-! CurlB cdot B / (4*\pi*B^2); for twist
-privates(1)= sum(CurlBp*Bp)/(4.*pi*sum(Bp*Bp))
+! CurlB \cdot B / (4*\pi*B^2); for twist
+privates(1)= dot_product(CurlBp, Bp)/(4.*pi*dot_product(Bp,Bp))
 
  ! |CurlB|^2
-privates(2)= sum(CurlBp*CurlBp)
+privates(2)= dot_product(CurlBp,CurlBp)
 
 ! |CurlB|/|B|; norm2s() is faster than norm2()
-privates(3)= norm2s(CurlBp)/norm2s(Bp) 
+privates(3)= norm2s(CurlBp)/norm2s(Bp)
 
-! A \ cdot b
-! privates(4) = sum (Ap*Bp/ norm2s (Bp))
+! A \cdot b
+! privates(4) = dot_product(Ap, Bp/ norm2s(Bp))
 end function privates
