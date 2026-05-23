4a85e4f9f06d24413cab7dd5c09005d4f88d33b0
el2718 <45861675+el2718@users.noreply.github.com>
2026-05-23 16:50:12 +0800

    Add files via upload

--- privates.f90
+++ privates.f90
@@ -9,29 +9,34 @@ real, pointer:: Ap(:)     ! the additional field at vp
 if (site%yinFlag) then
 ! if spherical, and |latitude=site%v(1)| .ge. lat_pole
     vp=site%v_yin(0:2)
-    Bp =>site%B_yin
-    if (site%CurlBFlag) CurlBp =>site%CurlB_yin
-    if (site%AFlag) Ap =>site%A_yin
+    Bp => site%B_yin
+    CurlBp => site%CurlB_yin
+    Ap => site%A_yin
 else
     vp=site%v(0:2)
-    Bp =>site%B
-    if (site%CurlBFlag) CurlBp =>site%CurlB
-    if (site%AFlag) Ap =>site%A
+    Bp => site%B
+    CurlBp => site%CurlB
+    Ap => site%A
 endif
 !------------------------------------------------------------
 ! for length
+if (int_private_out(0)) &
 site%private(0)= 1.
 
 ! CurlB \cdot B / (4*\pi*B^2); for twist
+if (int_private_out(1)) &
 site%private(1)= dot_product(CurlBp, Bp)/(4.*pi*dot_product(Bp, Bp))
 
- ! |CurlB|^2
+! |CurlB|^2
+if (int_private_out(2)) &
 site%private(2)= dot_product(CurlBp, CurlBp)
 
 ! |CurlB|/|B|; norm2s() is faster than norm2()
+if (int_private_out(3)) &
 site%private(3)= norm2s(CurlBp)/norm2s(Bp)
 
 ! A \cdot b
+! if (int_private_out(4)) &
 ! site%private(4) = dot_product(Ap, Bp/norm2s(Bp))
 end subroutine privates
 
