0f3d6f70a7692e2c07306a483f1680899860f7fb
el2718 <45861675+el2718@users.noreply.github.com>
2026-09-15 14:40:30 +0800

    Add files via upload

--- demo_hmi.py
+++ demo_hmi.py
@@ -2,62 +2,77 @@
 Download a synoptic HMI magnetogram and calculate the squashing factor Q by calling FastQSL directly from Python
 This demo is modified from HMI_Example.py of https://github.com/Valentin-Aslanyan/UFiT
 """
-import pfsspy, sunpy, wget, os
+import pfsspy, sunpy, wget, os, pickle
 import astropy.units
 import numpy as np
 from fastqsl import fastqsl
 # ------------------------------------------------------------
-def get_HMI_magnetogram(cr_target,data_dir):
-	if cr_target<2096:
-		print("CR "+str(cr_target)+" too early for HMI")
-		return ""
-	target_filename='hmi.Synoptic_Mr.'+str(cr_target)+'.fits'
-	
-	if target_filename not in os.listdir(data_dir):
-		wget.download('http://jsoc.stanford.edu/data/hmi/synoptic/'+target_filename, out=data_dir)
-	if target_filename in os.listdir(data_dir):
-		return os.path.join(data_dir,target_filename)
-	else:
-		print("Could not get HMI map for CR "+str(cr_target))
-		return ""
-# ------------------------------------------------------------
-target_CR=2284	#Carrington rotation
+def HMI_pfss4fastqsl(num_CR, num_r, num_t, num_p, Rss, data_dir):
+    Bfile='B4fastqsl'+str(num_CR)+'.pkl'
+    if Bfile not in os.listdir(data_dir):
+        
+        if num_CR<2096: raise Exception("CR "+str(num_CR)+" too early for HMI")
+        
+        HMI_file='hmi.Synoptic_Mr.'+str(num_CR)+'.fits'
+        
+        if HMI_file not in os.listdir(data_dir):
+            wget.download('http://jsoc.stanford.edu/data/hmi/synoptic/'+HMI_file, out=data_dir)
+            if HMI_file not in os.listdir(data_dir): 
+                raise Exception("Could not get HMI map for CR "+str(num_CR))
+            
+        HMI_map = sunpy.map.Map(HMI_file)
+        
+        # Downsample and remove NaNs as required by pfsspy
+        HMI_map = HMI_map.resample([num_p, num_t] * astropy.units.pix)
+        HMI_map.data[np.isnan(HMI_map.data)]=0.0    # NaNs set to zero
 
-num_r=60	#dimensions of B grid
+        pfss_in  = pfsspy.Input(HMI_map, num_r, Rss)
+        pfss_out = pfsspy.pfss(pfss_in)
+
+        b_lon= pfss_out.bg[:,:,:,0].transpose(2,1,0)
+        b_lat=-pfss_out.bg[:,:,:,1].transpose(2,1,0)
+        b_r  = pfss_out.bg[:,:,:,2].transpose(2,1,0)
+
+        lon_rad= pfss_out.grid.pg
+        lat_rad= np.arcsin(pfss_out.grid.sg)
+        radius = np.exp(pfss_out.grid.rg)
+    
+        with open(Bfile, 'wb') as file: 
+            pickle.dump((b_lon, b_lat, b_r, lon_rad, lat_rad, radius), file)
+    return Bfile
+# ------------------------------------------------------------
+num_CR=2281    # Carrington rotation
+
+num_r=60    # dimensions of B grid
 num_t=180
 num_p=360
 
-Rss=2.5		#Source surface
-cdir = os.getcwd()+os.sep 
-HMI_data_dir=cdir
+Rss=2.5        # the radius of Source surface
+
+data_dir = os.getcwd()+os.sep
 # ------------------------------------------------------------
-#Download magnetogram fits file
-HMI_filename=get_HMI_magnetogram(target_CR, HMI_data_dir)
-HMI_map = sunpy.map.Map(HMI_filename)
-
-#Downsample and remove NaNs as required by pfsspy
-HMI_map = HMI_map.resample([num_p, num_t] * astropy.units.pix)
-HMI_map.data[np.isnan(HMI_map.data)]=0.0	#NaNs set to zero
-
-#Compute B from PFSS and transform the field and coordinates into required form
-pfss_in = pfsspy.Input(HMI_map, num_r, Rss)
-pfss_out = pfsspy.pfss(pfss_in)
+Bfile= HMI_pfss4fastqsl(num_CR, num_r, num_t, num_p, Rss, data_dir)
+with open(data_dir+Bfile, "rb") as file:
+    b_lon, b_lat, b_r, lon_rad, lat_rad, radius = pickle.load(file)
 # ------------------------------------------------------------
-# compute Q with FastQSL
-
-b_r  = pfss_out.bg[:,:,:,2].transpose(2,1,0)
-b_lat=-pfss_out.bg[:,:,:,1].transpose(2,1,0)
-b_lon= pfss_out.bg[:,:,:,0].transpose(2,1,0)
-
-radius=np.exp(pfss_out.grid.rg)
-lat_rad=np.arcsin(pfss_out.grid.sg)
-lon_rad=pfss_out.grid.pg
-
+# compute Q at bottom
 fastqsl(b_lon, b_lat, b_r, \
 xa=lon_rad, ya=lat_rad, za=radius, spherical=True, \
-preview=True)
+fname='pfss_orig', preview=True, keep_tmp=True)
 
+# remove first two layers to remove small scale structure
 r_cut=2
 fastqsl(b_lon[r_cut:,:,:], b_lat[r_cut:,:,:], b_r[r_cut:,:,:], \
 xa=lon_rad, ya=lat_rad, za=radius[r_cut:], spherical=True, \
-fname='r_cut2', preview=True)
+fname='pfss_rcut2', preview=True, keep_tmp=True)
+
+# trace field lines from two points
+# Since keep_tmp=True was set in the command above, bfield.bin has already been saved in tmp_dir; 
+# therefore, the input magnetic field is unnecessary here
+qsl=fastqsl(\
+# b_lo[r_cut:,:,:], b_lat[r_cut:,:,:], b_r[r_cut:,:,:], \
+# xa=lon_rad, ya=lat_rad, za=radius[r_cut:], spherical=True, \
+fname='pfss_rcut2_seed_path', preview=True, \
+length_out=True, \
+seed=[[np.pi*0.85, 0.1, 1.7], [np.pi*1.5, -0.2, 1.2]], \
+path_out=True, loopB_out=True)
