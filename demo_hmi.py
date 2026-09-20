faa2111ae680a6db7d3bfba14d0bb4adc1b5faeb
el2718 <45861675+el2718@users.noreply.github.com>
2026-09-20 17:43:53 +0800

    Add files via upload

--- demo_hmi.py
+++ demo_hmi.py
@@ -12,7 +12,7 @@ def pfss4fastqsl(num_CR, *, \
                  data_type="hmi.Synoptic_Mr_polfil", \
                  data_dir=os.getcwd()+os.sep, fname=None, \
                  ):
-    if fname is None: fname = 'Bvec_'+data_type+'.'+str(num_CR)
+    if fname is None: fname = 'pfss_'+data_type+'.'+str(num_CR)
 
     Bfile = data_dir+fname+'.pkl'
     if fname+'.pkl' not in os.listdir(data_dir):
