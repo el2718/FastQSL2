1fe2df1f7f7d713c55930800249047172d358259
el2718 <45861675+el2718@users.noreply.github.com>
2026-09-20 17:42:40 +0800

    Add files via upload

--- demo_hmi.py
+++ demo_hmi.py
@@ -7,79 +7,78 @@ import astropy.units
 import numpy as np
 from fastqsl import fastqsl
 # ------------------------------------------------------------
-def HMI_pfss4fastqsl(num_CR, num_r, num_t, num_p, Rss, data_dir):
-    Bfile='B4fastqsl'+str(num_CR)+'.pkl'
-    if Bfile not in os.listdir(data_dir):
+def pfss4fastqsl(num_CR, *, \
+                 Rss=2.5, n_lon=361, n_lat=181, n_r=76, \
+                 data_type="hmi.Synoptic_Mr_polfil", \
+                 data_dir=os.getcwd()+os.sep, fname=None, \
+                 ):
+    if fname is None: fname = 'Bvec_'+data_type+'.'+str(num_CR)
+
+    Bfile = data_dir+fname+'.pkl'
+    if fname+'.pkl' not in os.listdir(data_dir):
         
         if num_CR<2096: raise Exception("CR "+str(num_CR)+" too early for HMI")
-        
-        HMI_file='hmi.Synoptic_Mr.'+str(num_CR)+'.fits'
-        
+        HMI_file=data_type+'.'+str(num_CR)+'.fits'
         if HMI_file not in os.listdir(data_dir):
             wget.download('http://jsoc.stanford.edu/data/hmi/synoptic/'+HMI_file, out=data_dir)
             if HMI_file not in os.listdir(data_dir): 
                 raise Exception("Could not get HMI map for CR "+str(num_CR))
-            
-        HMI_map = sunpy.map.Map(HMI_file)
-        
-        # Downsample and remove NaNs as required by pfsspy
-        HMI_map = HMI_map.resample([num_p, num_t] * astropy.units.pix)
-        HMI_map.data[np.isnan(HMI_map.data)]=0.0    # NaNs set to zero
 
-        pfss_in  = pfsspy.Input(HMI_map, num_r, Rss)
+        HMI_map = sunpy.map.Map(HMI_file)
+
+        # Downsample and remove NaNs as required by pfsspy
+        HMI_map = HMI_map.resample([n_lon-1, n_lat-1] * astropy.units.pix)
+        HMI_map.data[np.isnan(HMI_map.data)]=0.0    # NaNs set to zero
+        pfss_in  = pfsspy.Input(HMI_map, n_r-1, Rss)
         pfss_out = pfsspy.pfss(pfss_in)
 
         # exchange the index order of R and phi (longitude)
-        bvec= pfss_out.bg.transpose(2,1,0,3) 
+        Bvec= pfss_out.bg.transpose(2,1,0,3) 
 
-        # b_lat = -b_theta
-        bvec[:,:,:,1]= - bvec[:,:,:,1]
+        # # b_lat = -b_theta
+        Bvec[:,:,:,1]= - Bvec[:,:,:,1]
 
         lon_rad= pfss_out.grid.pg
         lat_rad= np.arcsin(pfss_out.grid.sg)
         radius = np.exp(pfss_out.grid.rg)
     
         with open(Bfile, 'wb') as file: 
-            pickle.dump((bvec, lon_rad, lat_rad, radius), file)
+            pickle.dump((Bvec, lon_rad, lat_rad, radius), file)
+
+        print(Bfile+' is saved')
     return Bfile
 # ------------------------------------------------------------
 # Carrington rotation
-num_CR=2284
+num_CR= 2284
 
-# dimensions of B grid
-num_r=60
-num_t=180
-num_p=360
+data_type="hmi.Synoptic_Mr_polfil" 
+# data_type="hmi.Synoptic_Mr" 
+# data_type="hmi.Synoptic_Ml"
+# data_type="hmi.Synoptic_Ml_small"
+# data_type="hmi.B_synoptic_Br"
+# data_type="hmi.B_synoptic_Br_small"
 
-# the radius of Source surface
-Rss=2.5
-
-# the directory to save the magnetic field
-data_dir = os.getcwd()+os.sep
-# ------------------------------------------------------------
-Bfile= HMI_pfss4fastqsl(num_CR, num_r, num_t, num_p, Rss, data_dir)
-with open(data_dir+Bfile, "rb") as file:
-    bvec, lon_rad, lat_rad, radius = pickle.load(file)
+Bfile= pfss4fastqsl(num_CR, data_type=data_type)
+with open(Bfile, "rb") as file:
+    Bvec, lon_rad, lat_rad, radius = pickle.load(file)
 # ------------------------------------------------------------
 r_cut=2
+fname = 'Bvec_'+data_type+'.'+str(num_CR)
 
-# compute Q at bottom
-fastqsl(bvec, xa=lon_rad, ya=lat_rad, za=radius, spherical=True, \
-fname='pfss_orig', preview=True, keep_tmp=True)
+# # compute Q at bottom
+fastqsl(Bvec, xa=lon_rad, ya=lat_rad, za=radius, spherical=True, \
+fname= fname, preview=True, keep_tmp=True)
 
-# remove first two layers to remove small scale structure
-fastqsl(bvec[r_cut:,:,:, :], xa=lon_rad, ya=lat_rad, za=radius[r_cut:], spherical=True, \
-fname='pfss_rcut2', scottFlag=False, preview=True, keep_tmp=True)
-
-# fastqsl(bvec[r_cut:,:,:, :], xa=lon_rad, ya=lat_rad, za=radius[r_cut:], spherical=True, \
-# fname='pfss_rcut2_sc', scottFlag=True, preview=True, keep_tmp=True)
+# # remove first two layers to remove small scale structure
+fastqsl(Bvec[r_cut:,:,:, :], xa=lon_rad, ya=lat_rad, za=radius[r_cut:], spherical=True, \
+fname= fname+'_rcut2', scottFlag=False, preview=True, keep_tmp=True)
 
 # trace field lines from two points
 # Since keep_tmp=True was set in the command above, bfield.bin has already been saved in tmp_dir; 
 # therefore, the input magnetic field is unnecessary here
 qsl=fastqsl(\
-# bvec[r_cut:,:,:, :], xa=lon_rad, ya=lat_rad, za=radius[r_cut:], spherical=True, \
-fname='pfss_rcut2_seed_path', preview=True, \
+# Bvec[r_cut:,:,:, :], xa=lon_rad, ya=lat_rad, za=radius[r_cut:], spherical=True, \
+fname= fname+'_rcut2_seed_path', preview=True, \
 length_out=True, \
 seed=[[np.pi*0.85, 0.1, 1.7], [np.pi*1.5, -0.2, 1.2]], \
 path_out=True, loopB_out=True)
@@ -87,5 +86,5 @@ path_out=True, loopB_out=True)
 # # git clone https://github.com/el2718/par2solarwind
 # from par2solarwind import par2solarwind
 # # compute two parameters for solar wind modeling at bottom
-# par2solarwind(bvec[r_cut:,:,:, :], lon_rad, lat_rad, radius[r_cut:], \
-#               bottomFlag=True, fname='pfss_solarwind', preview=True)
+# par2solarwind(Bvec[r_cut:,:,:, :], lon_rad, lat_rad, radius[r_cut:], \
+#               bottomFlag=True, fname='solarwind', preview=True)
